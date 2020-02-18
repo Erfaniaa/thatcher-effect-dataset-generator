02ec5f73f64381ffd911cc8a65c3deb4ab625723
Erfan Alimohammadi <erfan.aa@gmail.com>
2020-02-18 04:27:39 +0330

    Flip and show images

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ INPUT_IMAGES_DIRECTORY_PATH = "input_images"
 ATTRIBUTES_CSV_PATH = "attributes.csv"
 ATTRIBUTES_CSV_DELIMITER = ","
 
+def flip_subrectangle(image, x1, y1, x2, y2):
+        mid_x = (x1 + x2) // 2
+        for x in range(x1, mid_x):
+                for y in range(y1, y2):
+                        image[x][y], image[2 * mid_x - x][y] = image[2 * mid_x - x][y].copy(), image[x][y].copy()
+
 dataset_directory_filenames = listdir(INPUT_IMAGES_DIRECTORY_PATH)
 dataset_directory_filenames.sort()
 
-for image_id in range(1, 15):
+for image_id in range(1, 80):
 
         filename = dataset_directory_filenames[image_id]
 
@@ -33,14 +39,13 @@ for image_id in range(1, 15):
         cols = len(csv_contents.iloc[0])
 
         x, y = csv_contents.iloc[image_id]["lefteye_x"], csv_contents.iloc[image_id]["lefteye_y"]
-        cv2.rectangle(image, (x - 13, y - 6), (x + 13, y + 8), (0, 0, 255), 1)
+        flip_subrectangle(image, y - 6, x - 13, y + 8, x + 13)
         x, y = csv_contents.iloc[image_id]["righteye_x"], csv_contents.iloc[image_id]["righteye_y"]
-        cv2.rectangle(image, (x - 13, y - 6), (x + 13, y + 8), (0, 0, 255), 1)
+        flip_subrectangle(image, y - 6, x - 13, y + 8, x + 13)
         x1, y1 = csv_contents.iloc[image_id]["leftmouth_x"], csv_contents.iloc[image_id]["leftmouth_y"]
-        # cv2.rectangle(image, (x1, y1), (x1, y1), (0, 0, 255), 1)
         x2, y2 = csv_contents.iloc[image_id]["rightmouth_x"], csv_contents.iloc[image_id]["rightmouth_y"]
-        # cv2.rectangle(image, (x2, y2), (x2, y2), (0, 0, 255), 1)
-        cv2.rectangle(image, (x1 - 2, y1 - 5), (x2 + 2, y2 + 15), (0, 0, 255), 1)
+        flip_subrectangle(image, y1 - 6, x1 - 2, y2 + 16, x2 + 2)
+        image = cv2.flip(image, 0)
         cv2.imshow("image " + str(image_id), image)
 
 cv2.waitKey(0)
